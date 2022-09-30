a11632ad930cd67a09911f9e36003aec6c6ec8a6
SantiRagnar <santbarrios130600@gmail.com>
2022-09-30 12:29:26 -0300

    cambios

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let acc = ''
 
 function mostrarPersonaje(personaje) {
-    acc += `
+	acc += `
     <div class='cont-personaje'>
       <div class="cont-img">
         <img src="${personaje.image}"/>
@@ -10,9 +10,33 @@ function mostrarPersonaje(personaje) {
         <h5 class="name">${personaje.name}</h5>
         <p class="gender">genero: ${personaje.gender}</p>
         <p class="species">especie: ${personaje.species}</p>
-        <p class="status">status:${personaje.status}</p>
+        <p class="status">status: ${personaje.status}</p>
         </div>
      </div>
+    `
+}
+
+function mostrarLocalidad(localidad) {
+	acc += `
+    <div class='cont-localidad'>
+      <div class="cont-info">
+        <h5 class="name">${localidad.name}</h5>
+			  <p class="gender"><span>tipo:</span> ${localidad.type}</p>
+        <p class="species"><span>dimension:</span> ${localidad.dimension}</p>
+      </div>
+     </div>
+    `
+}
+
+function mostrarEpisodio(episodio) {
+	acc += `
+    <div class='cont-episodio'>
+      <div class="cont-info">
+        <h5 class="name">${episodio.name}</h5>
+        <p class="fecha">fecha: ${episodio.air_date}</p>
+        <p class="episodio"><span>episodio:</span> ${episodio.episode}</p>
+      </div>
+    </div>
     `
 }
 
@@ -20,52 +44,76 @@ let personaje = document.querySelector('.personajes')
 let localidades = document.querySelector('.localidades')
 let episodios = document.querySelector('.episodios')
 
-let arrPersonajes = []
-let arrLocalidades = []
-let arrEpisodios = []
-let resultados = document.getElementById('resultados')
+let arrPersonajes = [];
+let arrLocalidades = [];
+let arrEpisodios = [];
+let resultados = document.getElementById('resultados');
+let resultadosLoc = document.getElementById('resultadosLoc');
+let resultadosEpi = document.getElementById('resultadosEpi');
 
-console.log(personaje)
+console.log("acc", acc)
 
 personaje.addEventListener('click', (e) => {
-    console.log('click')
-    fetch(`https://rickandmortyapi.com/api/character`)
-        .then(res => res.json())
-        .then(data => {
-            arrPersonajes = [...data.results];
-            console.log(arrPersonajes)
-            arrPersonajes.map((personaje) => {
-                mostrarPersonaje(personaje)
-            })
-            resultados.innerHTML = acc;
-        })
-        .catch(err => { console.log(err) })
+	console.log('click')
+	fetch(`https://rickandmortyapi.com/api/character`)
+		.then(res => res.json())
+		.then(data => {
+			arrPersonajes = [...data.results];
+
+			resultadosLoc.classList.add("deshabilitar");
+			resultadosEpi.classList.add("deshabilitar");
+			resultados.classList.remove("deshabilitar");
+			resultados.innerHTML = "";
+
+			arrPersonajes.map((personaje) => {
+				mostrarPersonaje(personaje)
+			})
+
+			resultados.innerHTML = acc;
+			acc = "";
+		})
+		.catch(err => { console.log(err) })
 })
 
 localidades.addEventListener('click', (e) => {
-    fetch(`https://rickandmortyapi.com/api/location`)
-        .then(res => res.json())
-        .then(data => {
-            arrLocalidades = [...data.results];
-            console.log(arrLocalidades)
-            arrLocalidades.map((localidad) => {
-                //mostrarPersonaje(localidad)
-            })
-            resultados.innerHTML = acc;
-        })
-        .catch(err => { console.log(err) })
+	fetch(`https://rickandmortyapi.com/api/location`)
+		.then(res => res.json())
+		.then(data => {
+			arrLocalidades = [...data.results];
+
+			resultados.classList.add("deshabilitar")
+			resultadosEpi.classList.add("deshabilitar");
+			resultadosLoc.classList.remove("deshabilitar");
+			resultadosLoc.innerHTML = "";
+
+			arrLocalidades.map((localidad) => {
+				mostrarLocalidad(localidad)
+			})
+
+			resultadosLoc.innerHTML = acc;
+			acc = "";
+		})
+		.catch(err => { console.log(err) })
 })
 
 episodios.addEventListener('click', (e) => {
-    fetch(`https://rickandmortyapi.com/api/episode`)
-        .then(res => res.json())
-        .then(data => {
-            arrEpisodios = [...data.results];
-            console.log(arrEpisodios)
-            arrEpisodios.map((episodio) => {
-                //mostrarPersonaje(episodio)
-            })
-            resultados.innerHTML = acc;
-        })
-        .catch(err => { console.log(err) })
+	fetch(`https://rickandmortyapi.com/api/episode`)
+		.then(res => res.json())
+		.then(data => {
+			arrEpisodios = [...data.results];
+
+			resultados.classList.add("deshabilitar")
+			resultadosLoc.classList.add("deshabilitar")
+			resultadosEpi.classList.remove("deshabilitar");
+
+			resultadosEpi.innerHTML = "";
+
+			arrEpisodios.map((episodio) => {
+				mostrarEpisodio(episodio)
+			})
+
+			resultadosEpi.innerHTML = acc;
+			acc = ""
+		})
+		.catch(err => { console.log(err) })
 })
